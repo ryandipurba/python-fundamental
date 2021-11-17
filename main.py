1afd3654bd1f1bb5185a0acee25b381c59c55880
ryandi purba <purbaryandi@gmail.com>
2021-11-17 21:06:57 +0700

    conditional and loop

---
+++ main.py
@@ -0,0 +1,28 @@
+# if
+
+numbers = 8
+
+if numbers < 10:
+    print(f'{numbers} is less than 10')
+elif numbers > 10:
+    print(f'{numbers} is greater than 10')
+else:
+    print(f'{numbers} equal 10')
+
+# for
+print("\nloop")
+number = [1, 2, 3, 4, 5]
+for x in number:
+    print(x)
+
+fruits = ["pineapple", "apple", "orange"]
+for food in fruits:
+    print("I like eat", food)
+
+print('\nwhile loop')
+count = 0
+while count < 9:
+    print("The count is: ", count)
+    count = count + 1
+
+print("Good bye!")
